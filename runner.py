95c812331bfbeb465cb4dea0af7fbcc99e2ab95f
Juan Canon <canonjp@javeriana.edu.co>
2025-08-17 15:32:25 -0500

    feat: acciones posibles

--- runner.py
+++ runner.py
@@ -30,4 +30,29 @@ def funcion_heuristica(estado):
     litros_jarra5, litros_jarra4 = estado
     return abs(litros_jarra5 - 2)
 
+# 3) Acciones y precondiciones
+
+def obtener_acciones_posibles(estado):
+    """
+    Retorna el conjunto de acciones posibles para aplicar en el 'estado'
+    Se verifican las precondiciones básicas para cada posible acción
+    """
+    litros_jarra5, litros_jarra4 = estado
+    conjunto_acciones = set()
+
+    if litros_jarra5 < CAPACIDAD_JARRA5:
+        conjunto_acciones.add("L5")
+    if litros_jarra4 < CAPACIDAD_JARRA4:
+        conjunto_acciones.add("L4")
+    if litros_jarra5 > 0:
+        conjunto_acciones.add("V5")
+    if litros_jarra4 > 0:
+        conjunto_acciones.add("V4")
+    if litros_jarra5 > 0 and litros_jarra4 < CAPACIDAD_JARRA4:
+        conjunto_acciones.add("T54")
+    if litros_jarra4 > 0 and litros_jarra5 < CAPACIDAD_JARRA5:
+        conjunto_acciones.add("T45")
+
+    return conjunto_acciones
+
 
