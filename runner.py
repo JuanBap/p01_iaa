e1744c6452b582ae7ccf4f6572146b87079d3404
Juan Canon <canonjp@javeriana.edu.co>
2025-08-17 15:24:23 -0500

    feat: función de estado inicial

---
+++ runner.py
@@ -0,0 +1,14 @@
+from heapq import heappush, heappop
+
+# Capacidades de las jarras
+CAPACIDAD_JARRA5 = 5
+CAPACIDAD_JARRA4 = 4
+
+# 1) Estado inicial y final
+
+def obtener_estado_inicial():
+    """
+    Devuelve el estado inicial (las dos jarras están vacías)
+    """
+    return (0,0)
+
